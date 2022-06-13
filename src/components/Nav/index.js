0cf7e18d81cd6acb708c8ce6da4f513bf77b78a5
jennj <jennjenn93@gmail.com>
2022-06-13 13:30:43 -0500

    adding nav tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 
 function Nav() {
 const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
+    { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects"},
     { name: "portraits", description: "Portraits of people in my life" },
     { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
+    { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature" },
   ];
 
 
@@ -22,14 +15,14 @@ const categories = [
     return (
         <header>
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera">📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">
+                        <a data-testid="about" href="#about">
                             About me
                         </a>
                     </li>
